8cb6ca0dfb33bb7737e253fcc4051b01aea0320a
sachinshere <sachin.shere07@gmail.com>
2019-03-23 14:10:00 +0530

    Second Commit

--- src/hello.java
+++ src/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		System.out.println("Hello World");
 		System.out.println("Hello India");
 		System.out.println("Hello Maharashtra");
+		System.out.println("Hello Pune");
 	}
 
 }
